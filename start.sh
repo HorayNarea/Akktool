6ec2f5c1971c263296c636b2980cb22997881b1f
Thomas Sänger <HorayNarea@gmail.com>
2014-04-01 21:19:12 +0200

    bugfix for start-script

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 pip install -r requirements.txt
-if [[ -z "$1" ]]; then
+if [ "$1" != "" ]
+  then
+    echo $*
+    python manage.py syncdb
     python import.py $1
 fi
 python manage.py syncdb
