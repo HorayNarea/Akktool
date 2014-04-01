417b3d104a9b8f06deaa75bff9e78464f85b786d
Thomas Sänger <HorayNarea@gmail.com>
2014-04-02 00:31:29 +0200

    rudimentäres Qurorum unter $baseurl/quorum

--- akkreditierung/views.py
+++ akkreditierung/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from akkreditierung.models import Member
 
-# Create your views here.
+
+def quorum(request):
+    basis = len(Member.objects.filter(haspaid=1))
+    akk = len(Member.objects.filter(akk=1))
+    quorum = float(akk) / float(basis) * 100
+    context = {'basis': basis, 'akk': akk, 'quorum': quorum}
+    return render(request, 'akkreditierung/quorum.html', context)
