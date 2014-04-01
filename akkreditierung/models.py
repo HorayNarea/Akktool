f75db123ba550831ca330399ed1bf646834926cd
Thomas Sänger <HorayNarea@gmail.com>
2014-04-01 16:10:37 +0200

    added db-model

--- akkreditierung/models.py
+++ akkreditierung/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    middlename = models.CharField(max_length=500)
+    MemberID = models.PositiveIntegerField()
+    birthdate = models.DateField()
+    address = models.CharField(max_length=500)
+    city = models.CharField(max_length=100)
+    zipcode = models.PositiveIntegerField()
+    organisation = models.CharField(max_length=100)
+    haspayed = models.BooleanField()
--- akktool/settings.py
+++ akktool/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'akkreditierung',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -65,9 +66,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'de-de'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
