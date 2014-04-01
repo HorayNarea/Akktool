e88d3623b6347c57ded3b1055a1bc4cd5cd8fd4b
Thomas Sänger <HorayNarea@gmail.com>
2014-04-01 16:16:41 +0200

    added keywords & encoding

--- akkreditierung/models.py
+++ akkreditierung/models.py
@@ -1,13 +1,14 @@
+# coding=UTF-8
 from django.db import models
 
 class Member(models.Model):
-    firstname = models.CharField(max_length=100)
-    lastname = models.CharField(max_length=100)
-    middlename = models.CharField(max_length=500)
-    MemberID = models.PositiveIntegerField()
-    birthdate = models.DateField()
-    address = models.CharField(max_length=500)
-    city = models.CharField(max_length=100)
-    zipcode = models.PositiveIntegerField()
-    organisation = models.CharField(max_length=100)
-    haspayed = models.BooleanField()
+    firstname = models.CharField('Vorname', max_length=100)
+    lastname = models.CharField('Nachname', max_length=100)
+    middlename = models.CharField('zusätzliche Vornamen', max_length=500)
+    MemberID = models.PositiveIntegerField(primary_key=True)
+    birthdate = models.DateField('Geburtsdatum')
+    address = models.CharField('Anschrift', max_length=500)
+    city = models.CharField('Ort', max_length=100)
+    zipcode = models.PositiveIntegerField('PLZ')
+    organisation = models.CharField('LV', max_length=100)
+    haspayed = models.BooleanField('hat bezahlt?')
