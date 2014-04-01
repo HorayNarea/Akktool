cf5894af95869167e1384727b7a13c85cb2e8a56
Thomas Sänger <HorayNarea@gmail.com>
2014-04-01 20:36:43 +0200

    converter from http://www.machmaldieaugenauf.de/2012/01/06/akk-took/ csv-format to mine (BEWARE: all imported members will get "haspayed=True" !!!)

---
+++ convert.py
@@ -0,0 +1,13 @@
+import csv
+import sys
+
+
+inputfile = sys.argv[1]
+with open(inputfile, 'rb') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        dataset = [row[3], row[2], row[1], row[0], row[4], row[5], row[6], row[7], row[8], '1', '0', '0']
+        print ','.join(dataset)
+
+# csv-struktur
+#MemberID, firstname, middlename, lastname, birthdate, address, city, zipcode, organisation, haspayed, debt, akk
