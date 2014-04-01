13f9601f3cc3b159916374fb1b1cf87d1ab05651
Thomas Sänger <HorayNarea@gmail.com>
2014-04-02 00:47:26 +0200

    alle mitglieder auf quorums-seite

--- akkreditierung/views.py
+++ akkreditierung/views.py
@@ -3,8 +3,9 @@ from akkreditierung.models import Member
 
 
 def quorum(request):
+    mitglieder = len(Member.objects.all())
     basis = len(Member.objects.filter(haspaid=1))
     akk = len(Member.objects.filter(akk=1))
     quorum = float(akk) / float(basis) * 100
-    context = {'basis': basis, 'akk': akk, 'quorum': quorum}
+    context = { 'basis': basis, 'akk': akk, 'quorum': quorum, 'mitglieder': mitglieder }
     return render(request, 'akkreditierung/quorum.html', context)
