41792bcb485c808ba7f371af6b60079ac5c3daa6
Thomas Sänger <HorayNarea@gmail.com>
2014-04-02 03:26:56 +0200

    workaround for division by zero

--- akkreditierung/views.py
+++ akkreditierung/views.py
@@ -6,7 +6,16 @@ def quorum(request):
     mitglieder = len(Member.objects.all())
     basis = len(Member.objects.filter(haspaid=1))
     akk = len(Member.objects.filter(akk=1))
-    bezahlt = float(basis) / float(mitglieder) * 100
-    quorum = float(akk) / float(basis) * 100
+
+    if mitglieder == 0:
+        bezahlt = 0
+    else:
+        bezahlt = float(basis) / float(mitglieder) * 100
+
+    if basis == 0:
+        quorum = 0
+    else:
+        quorum = float(akk) / float(basis) * 100
+
     context = { 'basis': basis, 'akk': akk, 'quorum': round(quorum, 1), 'mitglieder': mitglieder , 'bezahlt': round(bezahlt, 1) }
     return render(request, 'akkreditierung/quorum.html', context)
