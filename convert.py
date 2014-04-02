d08588c95fafa208875bc67c69d1ad5619b95832
Thomas Sänger <HorayNarea@gmail.com>
2014-04-02 18:15:33 +0200

    updated converter

--- convert.py
+++ convert.py
@@ -6,8 +6,8 @@ inputfile = sys.argv[1]
 with open(inputfile, 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
-        dataset = [row[3], row[2], row[1], row[0], row[4], row[5], row[6], row[7], row[8], '1', '0', '0']
+        dataset = [row[3], row[2], row[1], row[0], row[4], row[5], row[6], row[7], row[8], row[9], '0', row[10]]
         print ','.join(dataset)
 
-# csv-struktur
+# output csv-structure
 #MemberID, firstname, middlename, lastname, birthdate, address, city, zipcode, organisation, haspaid, debt, akk
