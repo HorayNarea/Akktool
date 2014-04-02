a81a3f20b022675b87d438d76958d8e90656eb3a
Thomas Sänger <HorayNarea@gmail.com>
2014-04-02 02:37:19 +0200

    very editable list. much easy akkreditierung. wow.

--- akkreditierung/admin.py
+++ akkreditierung/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from akkreditierung.models import Member
 
+
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ('MemberID', 'firstname', 'middlename', 'lastname', 'haspaid', 'debt', 'akk')
     search_fields = ['MemberID', 'firstname', 'middlename', 'lastname', 'zipcode']
-    list_filter = ['akk']
+    list_display = ('MemberID', 'firstname', 'middlename', 'lastname', 'haspaid', 'debt', 'akk')
+    list_editable = ('haspaid', 'debt', 'akk')
+    list_filter = ['akk', 'haspaid']
 
 admin.site.register(Member, MemberAdmin)
