bfbacbb41e7e4258750cd163649a936391c2e440
Thomas Sänger <HorayNarea@gmail.com>
2014-04-03 19:05:34 +0200

    python3 kompatibel \o/

--- convert.py
+++ convert.py
@@ -2,12 +2,18 @@ import csv
 import sys
 
 
-inputfile = sys.argv[1]
-with open(inputfile, 'rb') as f:
-    reader = csv.reader(f)
+if len(sys.argv) < 2:
+    print('Usage:')
+    print('\t python ' + sys.argv[0] + ' /path/to/csvfile')
+    print('or:')
+    print('\t python ' + sys.argv[0] + ' /path/to/csvfile > converted.csv')
+else:
+    inputfile = sys.argv[1]
+    reader = csv.reader(open(inputfile, 'rt'))
     for row in reader:
         dataset = [row[3], row[2], row[1], row[0], row[4], row[5], row[6], row[7], row[8], row[9], '0', row[10]]
-        print ','.join(dataset)
+        print (','.join(dataset))
+
 
 # output csv-structure
 #MemberID, firstname, middlename, lastname, birthdate, address, city, zipcode, organisation, haspaid, debt, akk
--- import.py
+++ import.py
@@ -4,21 +4,21 @@ import sqlite3
 
 
 if len(sys.argv) < 2:
-    print 'Usage:'
-    print '\t python ' + sys.argv[0] + ' /path/to/csvfile'
+    print('Usage:')
+    print('\t python ' + sys.argv[0] + ' /path/to/csvfile')
 else:
+    linecounter = 0
     connection = sqlite3.connect('db.sqlite3')
     connection.text_factory = str
     db = connection.cursor()
     inputfile = sys.argv[1]
-    with open(inputfile, 'rb') as f:
-        reader = csv.reader(f)
-        linecounter = 0
-        for row in reader:
-            db.execute('INSERT INTO akkreditierung_member VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', row)
-            linecounter += 1
-        connection.commit()
-        print 'Imported ' + str(linecounter) + ' datasets!'
+    reader = csv.reader(open(inputfile, 'rt'))
+    for row in reader:
+        db.execute('INSERT INTO akkreditierung_member VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', row)
+        linecounter += 1
+    connection.commit()
+    print('Imported ' + str(linecounter) + ' datasets!')
+
 
 # csv-structure
 #MemberID,firstname,middlename,lastname,birthdate,address,city,zipcode,organisation,haspaid,debt,akk
